74d8ad51125eb96864e9163ff9a5364a62c3abf5
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-11-08 16:14:03 +0900

    main スタイリング

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import "./Main.css";
 
 const Main = () => {
-  return <div className="app-main">
-    <div className="app-main-note-edit">
-      <input type="text" />
-      <textarea id="" placeholder="ノート内容を記入"></textarea>
+  return (
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input type="text" />
+        <textarea id="" placeholder="ノート内容を記入"></textarea>
+      </div>
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">タイトル</h1>
+        <div className="markdown-preview">ああああ</div>
+      </div>
     </div>
-    <div className="app-main-note-preview">
-      <h1 className="preview-title">タイトル</h1>
-      <div className="markdown-preview"></div>
-    </div>
-  </div>;
+  );
 };
 
 export default Main;

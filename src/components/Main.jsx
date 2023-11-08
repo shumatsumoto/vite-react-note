97f6c0a41de41cf6db1849b4730d20869d7386c2
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-11-08 16:05:17 +0900

    component main作成

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./Main.css";
 
 const Main = () => {
-  return <div>Main</div>;
+  return <div className="app-main">
+    <div className="app-main-note-edit">
+      <input type="text" />
+      <textarea id="" placeholder="ノート内容を記入"></textarea>
+    </div>
+    <div className="app-main-note-preview">
+      <h1 className="preview-title">タイトル</h1>
+      <div className="markdown-preview"></div>
+    </div>
+  </div>;
 };
 
 export default Main;

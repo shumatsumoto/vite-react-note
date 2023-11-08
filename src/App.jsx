a18e78c10ff09c1a0de06b429a824dcc5f4543fb
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-11-08 13:44:53 +0900

    ノート出力

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <div className="App">
-      <Sidebar onAddNote={onAddNote} />
+      <Sidebar onAddNote={onAddNote} notes={notes} />
       <Main />
     </div>
   );
